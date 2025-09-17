2ba49f8189500ed04c64cd9685e149c894f9f8a6
CriusNyx <rjreynoldsw@gmail.com>
2025-09-17 16:41:30 -0400

    Implemented Damage System

--- scripts/ai/AI.cs
+++ scripts/ai/AI.cs
@@ -6,29 +6,8 @@ public partial class AI : Node3D
   [Export]
   public PackedScene bulletPrefab;
 
-  static IEnumerable<BulletSpawn> Pattern1 = new BulletSpawn { lifetime = 10, speed = 10f }
-    .Fan(10, 3, 0.6f)
-    .Fan(30, 5, 0.4f / 5);
-
-  static IEnumerable<BulletSpawn> Pattern2 = new BulletSpawn { lifetime = 10, speed = 10f }
-    .Fan(45, 5, 0)
-    .Repeat(3, 0.2f);
-
-  static IEnumerable<BulletSpawn> Pattern3 = new BulletSpawn
-  {
-    lifetime = 10,
-    speed = 10,
-  }.InterleavedFan(5, 75, 7, 0.4f, true);
-
-  static IEnumerable<BulletSpawn> Pattern5 = Pattern1.ThenSpawn(Pattern2).ThenSpawn(Pattern3, 1f);
-
   public override void _Ready()
   {
     base._Ready();
-    BulletSpawner.Create(
-      Pattern5,
-      new SpawnParameters { angle = 270, prefab = bulletPrefab },
-      this
-    );
   }
 }
--- scripts/damage/Hurtbox.cs
+++ scripts/damage/Hurtbox.cs
@@ -1,5 +1,68 @@
-using System;
 using Godot;
+using static Godot.GD;
 
 [GlobalClass]
-public partial class Hurtbox : CharacterBody3D { }
+public partial class Hurtbox : CharacterBody3D
+{
+  [Export]
+  public DamageFiler canBeHurtBy;
+  private CircleQueue<Hitbox> currentCollisions = new CircleQueue<Hitbox>(16);
+
+  public override void _Ready()
+  {
+    base._Ready();
+  }
+
+  private bool CanBeHurtBy(Hitbox other)
+  {
+    return (int)(canBeHurtBy & other.damageSource) != 0;
+  }
+
+  public override void _PhysicsProcess(double delta)
+  {
+    base._PhysicsProcess(delta);
+    if (MoveAndCollide(Vector3.Zero, true) is KinematicCollision3D collision)
+    {
+      CheckCollisionForDamage(collision);
+    }
+  }
+
+  private void CheckCollisionForDamage(KinematicCollision3D collision)
+  {
+    if (collision.GetCollider().As<Node>()?.GetParentOfType<Hitbox>() is Hitbox hitbox)
+    {
+      HandleProspectiveCollision(hitbox);
+    }
+  }
+
+  private void HandleProspectiveCollision(Hitbox hitbox)
+  {
+    if (CanBeHurtBy(hitbox))
+    {
+      if (!currentCollisions.Contains(hitbox))
+      {
+        currentCollisions.Enqueue(hitbox);
+        HandleHurt(hitbox);
+      }
+    }
+  }
+
+  private void HandleHurt(Hitbox hitbox)
+  {
+    hitbox.OnHit(this);
+    OnHurt(hitbox);
+  }
+
+  public void OnHurt(Hitbox hitbox)
+  {
+    foreach (var parent in this.GetParentsOfType<IHandleHurtboxEvents>())
+    {
+      parent.OnHurt(hitbox, this);
+    }
+  }
+}
+
+interface IHandleHurtboxEvents
+{
+  void OnHurt(Hitbox hitbox, Hurtbox hurtbox);
+}
